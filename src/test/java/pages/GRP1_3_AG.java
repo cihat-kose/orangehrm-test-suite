faf8b3d19b08bb0a0c47fde46c9e907e250f4795
AyseGun <aishagun2015@gmail.com>
2023-04-07 23:37:20 +0300

    My stepDefinition page and locater page of user stories GRP1_3-AG and GRP1_10-AG are completed

--- src/test/java/pages/GRP1_3_AG.java
+++ src/test/java/pages/GRP1_3_AG.java
@@ -1,8 +1,10 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 import utilities.GWD;
 
 // POM : Page Object Model
@@ -12,20 +14,53 @@ public class GRP1_3_AG extends Parent {
     }
 
 
-    @FindBy(xpath="(//mat-option//span)[2]")
-    private WebElement gradeLevel2;
+    @FindBy(css = "[name=username]")
+    public WebElement username;
 
-    public WebElement getWebElement(String strButton){
+    @FindBy(css = "[name=password]")
+    public WebElement password;
 
-        switch (strButton)
-        {
-            case "gradeLevel2" : return gradeLevel2;
-        }
-        return null;
-    }
+    @FindBy(xpath = "//button[@type='submit']")
+    public WebElement loginButton;
+
+    @FindBy(xpath = "(//span[@class='oxd-text oxd-text--span oxd-main-menu-item--name'])[1]")
+    public WebElement admin;
+
+    @FindBy(xpath = "//button[text()=' Add ']")
+    public WebElement addButton;
+
+    @FindBy(xpath = "(//i[@class='oxd-icon bi-caret-down-fill oxd-select-text--arrow'])[1]")
+    public WebElement selectRole;
+
+    @FindBy(xpath = "(//div[text()='-- Select --'])[1]")
+    public WebElement selectStatus;
+
+    @FindBy(xpath = "(//*[@type='password'])[1]")
+    public WebElement userPassword;
+
+    @FindBy(xpath = "(//*[@type='password'])[2]")
+    public WebElement userComPassword;
+
+    @FindBy(xpath = "//input[@placeholder='Type for hints...']")
+    public WebElement employeeName;
+
+    @FindBy(xpath = "(//input[@class='oxd-input oxd-input--active'])[2]")
+    public WebElement userUserName;
+
+    @FindBy(xpath = "//*[text()=' Save ']")
+    public WebElement saveButton;
+
+    @FindBy(xpath="//*[text()='Success']")
+    public WebElement successMessage;
+
+    @FindBy(xpath="//*[text()='Odis  Adalwin']")
+    public WebElement OdisAdalwin;
 
 
-    public void deleteItem(String searchedText) {
 
-    }
+
+
+
+
 }
+
