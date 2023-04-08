014c55418868143be311fb5416b1285a46daab43
ChatGTHB <cihat.kose@hotmail.com>
2023-04-08 14:14:30 +0300

    Duplicates resolved

--- src/test/java/stepDefinitions/GRP1_4_ASY_StepDefinitions.java
+++ src/test/java/stepDefinitions/GRP1_4_ASY_StepDefinitions.java
@@ -12,29 +12,25 @@ import utilities.GWD;
 public class GRP1_4_ASY_StepDefinitions {
 
     GRP1_4_ASY locators4= new GRP1_4_ASY();
-    @Given("Go to  mainpage")
-    public void goToMainpage() {
-
+    @Given("Go to  mainpage of the test object")
+    public void goToMainpageOfTheTestObject() {
         GWD.getDriver().get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
     }
 
-    @When("Write your admin username and password")
-    public void writeYourAdminUsernameAndPassword() {
-
+    @When("Write your admin username and password respectively")
+    public void writeYourAdminUsernameAndPasswordRespectively() {
         locators4.sendKeysFunction(locators4.username4, "Admin");
         locators4.sendKeysFunction(locators4.password4, "admin123");
         locators4.clickFunction(locators4.loginButton4);
     }
 
-    @Then("Click on Admin, user management page")
-    public void clickOnAdminUserManagementPage() {
-
+    @Then("Click on Admin, user management page on the website")
+    public void clickOnAdminUserManagementPageOnTheWebsite() {
         locators4.clickFunction(locators4.adminLink4);
     }
 
-    @And("Click on add button")
-    public void clickOnAddButton() {
-
+    @And("Press on add button to click")
+    public void pressOnAddButtonToClick() {
         locators4.clickFunction(locators4.addButton4);
     }
 
